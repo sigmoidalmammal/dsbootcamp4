8234e54522a7acdb515ba3ce449d847582aacf62
mvahit <m.vahitkeskin@gmail.com>
2021-02-04 20:01:06 +0100

    eda.py'a ekleme yapıldı

--- eda.py
+++ eda.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
+print("hello")
 #############################################
 # GENERAL
 #############################################
