1424fd5c2075d92e8848824b72e5c9ede50de368
Mürşide Yarkin <mursideyarkin@Murside-MacBook-Air.local>
2021-02-06 10:48:26 +0300

    isim eklendi

--- hi.py
+++ hi.py
@@ -1,5 +1,6 @@
 print("hi my name is mvk")
 print('02: Anil Gevrek')
 print("02: Melih Akdag")
-print(“03:  Pinar Dogan”)
+print("03:  Pinar Dogan")
 print("01: Ecem Bökenheide")
+print("03: Mürşide Yarkın")
